a24fb46069bd40a2f3872beec4ef02bbdf9cf9d7
Isaias <isaiaslflores@gmail.com>
2025-05-21 20:01:00 -0500

    server express

---
+++ README.md
@@ -0,0 +1,23 @@
+# Weather API
+
+JavaScript solution for [Weather API](https://roadmap.sh/projects/weather-api-wrapper-service) from [roadmap.sh](https://roadmap.sh/).
+
+## Requirements
+
+Must be installed:
+
+- Node.js
+- Redis (running)
+
+## How to run
+
+Execute the following steps:
+
+```bash
+# To install dependencies, run in console:
+$ npm install
+
+# To start the project, run in console:
+$ npm start
+
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import express from 'express'
+import { createClient } from 'redis'
+
+const app = express()
+const port = 3000
+const client = await createClient()
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
+app.get('/', (req, res) => {
+  res.json('Server listening, please enter param /location')
+})
+
+app.get('/:name', async (req, res) => {
+  const { name } = req.params
+  if (name) {
+    const data = await client.get(name)
+    if (data) {
+      return res.send(JSON.parse(data))
+    }
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}?unitGroup=us&key=ZS47EQ3ZQF5VDZA76PCA9HAZU&contentType=json`
+    let result
+    try {
+      result = await axios.get(url)
+      await client.set(name, JSON.stringify(result.data))
+      return res.send(result.data)
+    } catch (error) {
+      return res.json('Please enter a valid location.')
+    }
+  }
+  return res.json('Must enter a location.')
+})
+
+app.listen(port, () => {
+  console.log(`The server is listening on http://localhost:${port}`)
+})
